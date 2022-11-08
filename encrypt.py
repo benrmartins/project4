7b0bd524a1a8ad4269107b501f21bac94bebf962
benrmartins <benrmartins@gmail.com>
2022-11-08 17:15:12 -0500

    comments

--- encrypt.py
+++ encrypt.py
@@ -5,16 +5,23 @@ import sys
 
 # Entry point.
 def main():
+    # Accept n (int) and e (int) as command-line arguments
     n = int(sys.argv[1])
     e = int(sys.argv[2])
 
+    # Get the number of bits per character (call it width) needed for encryption, ie, number of bits needed to encode n
     width = rsa.bitLength(n)
+
+    # Accept message to encrypt from standard input.
     message = stdio.readAll()
 
     for i in range(len(message)):
+        # Use the built-in function ord() to turn c into an integer x.
         x = ord(message[i])
-        encrypt = rsa.encrypt(x, n, e)
-        binary = rsa.dec2bin(encrypt, width)
+        # encrypt the ninput
+        y = rsa.encrypt(x, n, e)
+        encrypt = rsa.dec2bin(y, width)
+        # Displayed the encrypted input
         stdio.write(binary)
     stdio.writeln()
 
--- keygen.py
+++ keygen.py
@@ -5,12 +5,16 @@ import sys
 
 # Entry point.
 def main():
+    # Accept lo (int) and hi (int) as command-line arguments
     lo = int(sys.argv[1])
     hi = int(sys.argv[2])
 
+    # Get public/private keys as a tuple.
     key = rsa.keygen(lo, hi)
 
+    # looped through the three values in the tuple, separated by a space.
     for i in range(len(key)):
+        # printed out the values separated by a space
         stdio.write(str(key[i]) + " ")
     stdio.writeln()
 
